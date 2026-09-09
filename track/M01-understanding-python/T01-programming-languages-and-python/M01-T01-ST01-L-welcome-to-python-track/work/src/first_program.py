fc5f0d63674a2ea90e415c9b350ce68f69ef3588
Vismitha974 <vismithav974@gmail.com>
2026-09-09 15:50:20 +0530

    Day7-Fundamentals

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/first_program.py
@@ -0,0 +1,24 @@
+print("This is my first program")
+
+#Assignment: Basic print() in Python
+# Task: Write a Python program that prints the following information:
+# Your name
+# Your age
+# Your city
+# Your favorite programming language
+# The message: In a paragraph about your 1 week of python Journey."
+
+print("Name : Vismitha")
+print("Age : 22")
+print("City : Shivamogga")
+print("Favorite programming language : Python")
+print("Message : I was very excited to start my python journey and explore the world of programming with this new and interesting track. and I learned the basics of Python like why it is useful from any other language.")
+
+
+# print * pattern
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
