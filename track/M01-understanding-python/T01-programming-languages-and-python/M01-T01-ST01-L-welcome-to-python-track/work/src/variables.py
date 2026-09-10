773fe322153ea6746e7898cb22f10b95f0aba97b
Vismitha974 <vismithav974@gmail.com>
2026-09-10 15:17:53 +0530

    Day8 -Variables

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/variables.py
@@ -0,0 +1,28 @@
+name = "Vismitha"
+# name = "Shama"
+#print(name)
+
+age = 21
+# print My name is chandu and age is 21
+print("My name is " + name + " and age is" , age,"!")
+print("My name is " + name + " and age is " + str(age) + "!")
+
+# formatting strings
+print(f"My name is {name} and age is {age}!")
+
+marks = 50
+marks = marks+10
+print(marks)
+
+a,b,c = 10,20,30
+print(a,b,c)
+
+x = y = z =100
+print(x,y,z)
+
+x=100
+y=30
+print(x+y)
+print(x*y)
+print(x-y)
+print(x/y)
